5b6d7adabea43aa7edca4f9194a2768d40e17f95
kapa16 <kapa16@inbox.ru>
2018-12-04 21:17:50 +0500

    did

--- Lesson5/task3.js
+++ Lesson5/task3.js
@@ -4,15 +4,46 @@
 const btn = document.querySelector('button');
 btn.addEventListener('click', checkForm);
 
-function checkForm(event) {
-  event.preventDefault();
-  const name = document.getElementById('name');
-  console.log(name);
-  if (name.value.length > 0 && name.value.length <= 50) {
-    name.classList.add('right');
-    name.classList.remove('error');
+function showErrorCorrect(elem, error) {
+  const label = elem.parentNode.querySelector('.form__label');
+  if (error) {
+    elem.classList.remove('right');
+    elem.classList.add('error');
+    label.classList.add('display_block');
   } else {
-    name.classList.add('error');
-    name.classList.remove('right');
+    elem.classList.add('right');
+    elem.classList.remove('error');
+    label.classList.remove('display_block');
   }
+}
+
+function checkForm(event) {
+
+  let error = true;
+
+  const name = document.getElementById('name');
+  let result = name.value.length <= 0 || name.value.length > 50;
+  error = error || result;
+  showErrorCorrect(name, result);
+
+  const phone = document.getElementById('phone');
+  const valuePhone = phone.value;
+  result = valuePhone.length !== 11 || valuePhone.replace(/\d/g, '').length !== 0;
+  error = error || result;
+  showErrorCorrect(phone, result);
+
+  const pass = document.getElementById('pass');
+  result = pass.value.length < 5 || pass.value.length > 50;
+  error = error || result;
+  showErrorCorrect(pass, result);
+
+  const confirmPass = document.getElementById('confirm-pass');
+  result = confirmPass.value !== pass.value;
+  error = error || result;
+  showErrorCorrect(confirmPass, result);
+//todo исправить error
+  if (error) {
+    event.preventDefault();
+  }
+
 }
