3120ce53076cddd0a0792b1be8286406f2b0c77f
Pavel <kapa16@inbox.ru>
2018-12-06 16:18:30 +0500

    did

--- Lesson5/task1.js
+++ Lesson5/task1.js
@@ -8,12 +8,12 @@ class Board {
 
   /**
    * Конструктор доски, с заданием размеров игрового поля
-   * @param {int} sizeX - количество строк
+   * @param {int} sizeRows - количество строк
    * @param {int} sizeY - количество колонок
    */
-  constructor(sizeX, sizeY) {
-    this.sizeX = sizeX;
-    this.sizeY = sizeY;
+  constructor(sizeRows, sizeCols) {
+    this.sizeRows = sizeRows;
+    this.sizeCols = sizeCols;
     this.playingField = [];
   }
 
@@ -22,32 +22,32 @@ class Board {
    * @param {object} elemBoard - HTML элемент <table>, в котором будет рисоваться доска
    */
   render(elemBoard) {
-    const sizeX = this.sizeX;
-    const sizeY = this.sizeY;
+    const sizeRows = this.sizeRows;
+    const sizeCols = this.sizeCols;
 
-    for (let row = 0; row < sizeX + 2; row++) {
+    for (let row = 0; row < sizeRows + 2; row++) {
 
       const tableRow = document.createElement('tr');
       elemBoard.appendChild(tableRow);
 
       const arrCells = [];
 
-      for (let col = 0; col < sizeY + 2; col++) {
+      for (let col = 0; col < sizeCols + 2; col++) {
         const cell = document.createElement('td');
 
         tableRow.appendChild(cell);
 
-        if (needSymbol(row, col, sizeX, sizeY)) {
+        if (needSymbol(row, col, sizeRows, sizeCols)) {
           cell.textContent = String.fromCharCode(96 + col);
-        } else if (needSymbol(col, row, sizeX, sizeY)) {
-          cell.textContent = sizeX - row + 1;
+        } else if (needSymbol(col, row, sizeRows, sizeCols)) {
+          cell.textContent = sizeRows - row + 1;
         }
 
-        if (row === 0 || col === 0 || row === sizeX + 1 || col === sizeY + 1) {
+        if (row === 0 || col === 0 || row === sizeRows + 1 || col === sizeCols + 1) {
           continue;
         }
 
-        cell.setAttribute('id', `${String.fromCharCode(96 + col)}${sizeX - row + 1}`);
+        cell.setAttribute('id', `${String.fromCharCode(96 + col)}${sizeRows - row + 1}`);
         arrCells.push(cell);
 
         cell.classList.add('main-field');
@@ -56,7 +56,7 @@ class Board {
           cell.classList.add('black');
         }
       }
-      if (row === 0 || row === sizeX + 1) {
+      if (row === 0 || row === sizeRows + 1) {
         continue;
       }
       this.playingField.push(arrCells);
