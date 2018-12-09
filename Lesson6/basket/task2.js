a949e90fd8b7f6ac4f4348fcba94ab5e39d3016f
kapa <kapa16@inbox.ru>
2018-12-09 23:15:08 +0500

    did

--- Lesson6/basket/task2.js
+++ Lesson6/basket/task2.js
@@ -1,27 +1,40 @@
 /*jslint node: true */
 "use strict";
 
+/**
+ * Объект списка продуктов
+ */
 const products = {
   productsArray: [],
 
+  /**
+   * Получает товары "с сервера"
+   */
   getProducts() {
-    for (let i = 0; i < 4; i++) {
+    for (let i = 1; i <= 4; i++) {
       const product = {
         id: i,
-        imagePath: `img/product${i + 1}.jpg`,
-        alt: `product${i + 1}`,
-        name: `Товар ${i + 1}`,
-        price: (i + 1) * 1000,
+        imagePath: `img/product${i}.jpg`,
+        alt: `product${i}`,
+        name: `Товар ${i}`,
+        price: (i) * 1000,
       };
 
       this.productsArray.push(product);
     }
   },
 
+  /**
+   * Выводит товары на страницу
+   */
   showProducts() {
     this.productsArray.forEach(product => this.createProductsCards(product))
   },
 
+  /**
+   * Создает элемент продукта на странице
+   * @param {object} product - объект товара для вывода на страницу
+   */
   createProductsCards(product) {
     const mainElement = document.querySelector('.products');
 
@@ -57,6 +70,9 @@ const products = {
   }
 };
 
+/**
+ * объект корзины
+ */
 const basket = {
   productsBasket: [],
   currentProduct: {
@@ -66,6 +82,7 @@ const basket = {
   countProducts: 0,
   sumProducts: 0,
   basketElement: document.querySelector('.basket'),
+  basketInfoElement: document.querySelector('.basket__info'),
   basketProductsElement: document.querySelector('.basket__products'),
 
   /**
@@ -79,7 +96,7 @@ const basket = {
     } else {
       this.calcCountProduct(1);
     }
-    this.createBasketElements();
+    this.showBasket();
   },
 
   /**
@@ -88,7 +105,7 @@ const basket = {
    */
   calcCountProduct(diff) {
     this.currentProduct.count += diff;
-    this.currentProduct.sum += this.currentProduct.price * this.currentProduct.count;
+    this.currentProduct.sum = this.currentProduct.price * this.currentProduct.count;
   },
 
   /**
@@ -96,7 +113,7 @@ const basket = {
    * @param {object} product - товар
    */
   addNewProductInBasket(product) {
-    this.currentProduct = Object.assign({}, product)
+    this.currentProduct = Object.assign({}, product);
     this.currentProduct.count = 0;
     this.currentProduct.sum = 0;
     this.calcCountProduct(1);
@@ -109,21 +126,97 @@ const basket = {
    * @returns {object} - найденный товар
    */
   findProductInBasket(product) {
-    return this.productsBasket.find(elem => elem.id = product.id);
+    return this.productsBasket.find(elem => elem.id === product.id);
   },
 
   /**
-   * Создает элементы корзины на странице
+   * Отображает информацию по корзине
    */
-  createBasketElements() {
+  showBasket() {
+    this.calcTotal();
     this.clearBasketOnPage();
-    const countElement = this.basketElement.querySelector('.basket__count-goods');
-    countElement.textContent = this.productsBasket.reduce((accum, element) => accum + element.count);
-    console.dir(countElement.textContent);
+    this.refreshBasketInfo();
+    this.refreshCountProducts();
+    this.productsBasket.forEach(product => this.addProductElement(product));
   },
 
+  /**
+   * Считает общее количество и сумму товаров в корзине
+   */
+  calcTotal() {
+    this.countProducts = this.productsBasket.reduce((accum, element) => accum + element.count, 0);
+    this.sumProducts = this.productsBasket.reduce((accum, element) => accum + element.sum, 0);
+  },
+
+  /**
+   * Удаляет содержимое корзины в HTML для вывода текущих товаров корзины
+   */
   clearBasketOnPage() {
     this.basketProductsElement.innerHTML = '';
+  },
+
+  /**
+   * Обновляет инофрмационное сообщение по содержимому корзины
+   */
+  refreshBasketInfo() {
+    if (this.countProducts === 0) {
+      this.basketInfoElement.textContent = 'Корзина пуста';
+    } else {
+      this.basketInfoElement.textContent = `Количество товаров в корзине: ${this.countProducts}, на сумму ${this.sumProducts}`;
+    }
+  },
+
+  /**
+   * Обновляет счетчик товаров в корзине
+   */
+  refreshCountProducts() {
+    const countElement = this.basketElement.querySelector('.basket__count-products');
+    countElement.textContent = this.countProducts;
+  },
+
+  /**
+   * Добавляет элемент продукта в корзину
+   * @param {object} product - объект товара для вывода на страницу
+   */
+  addProductElement(product) {
+    const productElement = document.createElement('div');
+    productElement.classList.add('basket__product');
+    productElement.dataset.productId = product.id;
+    this.basketProductsElement.appendChild(productElement);
+
+    const productImage = new Image(40, 45);
+    productImage.src = product.imagePath;
+    productElement.appendChild(productImage);
+
+    const productName = document.createElement('h5');
+    productName.textContent = product.name;
+    productName.classList.add('basket__product-name');
+    productElement.appendChild(productName);
+
+    const productSum = document.createElement('div');
+    productSum.textContent = `${product.count} x ${product.price} = ${product.sum}`;
+    productSum.classList.add('basket__product-sum');
+    productElement.appendChild(productSum);
+
+    const removeButton = document.createElement('div');
+    removeButton.classList.add('basket__product-remove');
+    removeButton.addEventListener('click', event => this.removeProduct(event));
+    productElement.appendChild(removeButton);
+  },
+
+  /**
+   * Удаляет товар из корзины
+   * @param {Event} event - событие нажатия на кнопку
+   */
+  removeProduct(event) {
+    const productId = +event.target.parentElement.dataset.productId;
+    for (let i = 0; i < this.productsBasket.length; i++) {
+      if (this.productsBasket[i].id === productId) {
+        this.productsBasket.splice(i, 1);
+        break;
+      }
+    }
+    this.showBasket();
   }
 };
 
@@ -131,9 +224,24 @@ const basket = {
 products.getProducts();
 products.showProducts();
 
+basket.basketElement.addEventListener('click', event => showBasket(event));
+
+function showBasket(event) {
+  if (!event.target.classList.contains('basket')) {
+    return;
+  }
+  if (basket.countProducts !== 0) {
+    basket.basketProductsElement.classList.toggle('hidden');
+  }
+}
+
+/**
+ * Обработчик добавления товаров по кнопке Купить
+ * @param {Event} event - событие нажатия на кнопку
+ */
 function basketHandler(event) {
   const handlerElement = event.target;
-  if (handlerElement.tagName != 'BUTTON') {
+  if (handlerElement.tagName !== 'BUTTON') {
     return;
   }
 
